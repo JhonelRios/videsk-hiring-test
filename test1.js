6216be28dcf6e9cccf32924d18752f9539dff474
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2021-12-06 18:10:13 -0500

    fix: test1 identation fix

--- test1.js
+++ test1.js
@@ -1,3 +1,3 @@
 module.exports = function Test1(server) {
-  return new Promise((resolve) => server.on('name', (data) => resolve(data)));
+    return new Promise(resolve => server.on('name', data => resolve(data)));
 };
